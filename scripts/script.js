a49b34aee6688bbafc5d49dbd26a7ada483bb1b9
Timmhart <63649558+Timmhart@users.noreply.github.com>
2021-01-21 14:11:24 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,11 @@ function onClickMenu() {
   document.getElementById('menu').classList.toggle("change");
   document.getElementById('nav').classList.toggle("change");
 }
+
+// menu = document.querySelector('nav');
+//
+// menu.addEventListener("click", ietsDoen);
+//
+// function ietsDoen() {
+//   menu.classList.add("nav", "change", "#bar");
+// }
